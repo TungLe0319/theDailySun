af954acf1d17b815d989ed255f08a37e959c2fb9
Tung N Le <tung.le0319@gmail.com>
2023-01-26 14:53:45 -0700

    setup

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@sidebase/nuxt-auth',
+    '@nuxtjs/tailwindcss',
+    '@huntersofbook/naive-ui-nuxt'
+  ],
+  typescript: {
+    shim: false
+  }
+})
