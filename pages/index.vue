69b2e4a64d0e932cdeb9216c43afcd445e188732
Tung N Le <tung.le0319@gmail.com>
2023-01-26 16:06:03 -0700

    tung

--- pages/index.vue
+++ pages/index.vue
@@ -10,9 +10,18 @@
 export default {
   setup () {
     onMounted(() => {
-
+getProducts()
     })
-
+async  function getProducts (){
+ try {
+    const res = await fetch('/products',{
+      method: "GET"
+    })
+    console.log(res);
+   } catch (error) {
+   console.log(error)
+   }
+}
     return {
 
     }
