66a2e1978e1aa2f5ef072f85629304957ce93527
Tung N Le <tung.le0319@gmail.com>
2023-01-29 01:06:07 -0700

    cartPage

---
+++ components/cart/CartProductCard.vue
@@ -0,0 +1,56 @@
+<template>
+   <div class="p-5 rounded-md my-4 flex justify-between product-card  transition-colors  " >
+  <div class="card flex">
+    <img   :src="product.img ||'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80'" alt="Product Image" class="product-image shadow-lg shadow-slate-400" >
+    <div class="ml-4">
+<h1 class="text-lg">{{product.title}}</h1>
+<h2 class="text-md font-bold text-gray-600">{{product.audience}}</h2>
+    </div>
+  </div>
+    <div class="flex    gap-x-20">
+    <p>product.Qty</p>
+    <p>${{product.price}}</p>
+
+
+  </div>
+</div>
+
+</template>
+
+
+<script>
+
+export default {
+  props: {
+    product: { type: Object, required: false }
+  },
+  setup () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.product-image{
+  height: 250px;
+  width: 175px;
+  object-fit: cover;
+
+border-radius: 8px;
+}
+.product-card:hover{
+transition: all 0.5 ease !important;
+ background-color: rgb(148 163 184 );
+img{
+   --tw-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.658), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+  --tw-shadow-colored: 0 10px 15px -3px var(--tw-shadow-color), 0 4px 6px -4px var(--tw-shadow-color);
+  box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+}
+}
+.product-card{
+transition: all 0.5 ease !important;
+}
+
+</style>
--- pages/cart.vue
+++ pages/cart.vue
@@ -1,93 +1,134 @@
 <template>
-
-    <div class=" pt-24 text-black p-5 bg-slate-300 h-screen relative">
-      <div class="  ">
-        <div class=" w-full md:w-1/2 h-full justify-center flex  ">
-<h1 class="text-5xl">My Cart {{products}}</h1>
+  <div class="pt-24 text-black p-5 bg-slate-300 h-screen relative">
+    <div class="  ">
+      <div class="w-full md:w-1/2 h-full justify-center flex">
+        <h1 class="text-5xl">My Cart</h1>
+      </div>
+      <div class="flex">
+        <div class="w-full md:w-2/3 p-5 px-14">
+          <div class="flex justify-between">
+            <div>
+              <p>Product</p>
+            </div>
+            <div class="flex mr-10 gap-x-20">
+              <p>Qty</p>
+              <p>Price</p>
+            </div>
+          </div>
+          <hr class="mt-3 bg-black p-1 rounded-lg " />
+          <div class="mt-4 relative overflow-y-auto scrollbar-container">
+            <div class="relative h-full">
+              <div class="p-4">
+                <CartProductCard v-for="i in products" :product="i" />
+              </div>
+            </div>
+          </div>
         </div>
-        <div class=" w-full md:w-2/3 p-5 px-14">
-<div class="flex justify-between ">
- <div >
-   <p>Product</p>
- </div>
-  <div class="flex ml-4   gap-x-12">
-    <p>Qty</p>
-    <p>Price</p>
-    <p>Total</p>
-  </div>
-</div>
-<hr  class="mt-3 bg-black p-1 rounded-lg">
-
-
-
-
-
-
-
-
+        <div class="w-full md:w-1/3 p-5 px-14 bg-slate-200 rounded-lg shadow-xl h-1/2">
+          <div class="flex flex-col gap-y-3">
+            <h1 class="text-2xl">Subtotal : </h1>
+            <h2 class="text-2xl text-slate-400">Est Shipping : </h2>
+            <h2 class="text-2xl text-slate-400">Est Sales Taxes : </h2>
+            <h2 class="text-2xl text-red-700">Order Total : </h2>
+          </div>
+          <div class=" mt-3">
+            <button class="p-3 w-full bg-zinc-900 text-white text-2xl font-bold rounded-sm shadow-lg">Begin
+              Checkout</button>
+          </div>
         </div>
       </div>
-
     </div>
-
-
+  </div>
 </template>
 <script>
-
 export default {
+  setup() {
+    const { data: products } = useFetch("/api/products/all");
 
-  setup () {
-
-
-    onMounted(() => {
-
-    })
-    const route = useRoute()
+    onMounted(() => { });
+    const route = useRoute();
 
     async function getUserCart() {
       try {
-
       } catch (error) {
-        logger.log(error)
+        logger.log(error);
       }
     }
 
-
-
     return {
+      products,
       route,
-      activeProduct: computed(() =>
-        AppState.activeProduct
-      ),
-
-
-    }
-  }
-}
+      activeProduct: computed(() => AppState.activeProduct),
+    };
+  },
+};
 </script>
 
-
-
 <style lang="scss" scoped>
-.product-image{
-  height: auto;
-  width: 100% !important;
-  object-fit: cover;
-   animation-duration: 1.5s;
-  animation-name: slidein;
-border-radius: 8px;
-}
-@keyframes slidein {
-  from {
-    margin-right: 100%;
-    width: 300%;
-    opacity: 0;
-    filter: blur(10px);
+.scrollbar-container {
+  /* Add overflow-y property to create a scrollbar on the y-axis */
+
+  /* Add webkit prefix to style the scrollbar */
+  -webkit-scrollbar {
+    width: 2px;
+    background-color: transparent;
   }
 
-  to {
-    margin-right: 0%;
-    width: 100%;
+  /* Style the thumb (draggable part of the scrollbar) */
+  -webkit-scrollbar-thumb {
+    border-radius: 1px;
+    background-color: #a0aec0;
   }
+
+  /* Add padding to hide the native scrollbar */
+  padding-right: 2px;
+}
+
+.relative {
+  position: relative;
+}
+
+.absolute {
+  position: absolute;
+}
+
+::-webkit-scrollbar {
+  width: 4px;
+  border-radius: 50em;
+  background-color: #a0aec0;
+}
+
+.overflow-y-auto {
+  overflow-y: auto !important;
+  height: 1000px;
+}
+
+.inset-y-0 {
+  top: 0;
+  bottom: 0;
+}
+
+.right-0 {
+  right: 0;
+}
+
+.w-5 {
+  width: 5px;
+}
+
+.h-full {
+  height: 100%;
+}
+
+.bg-gray-600 {
+  background-color: #9b9b9b;
+}
+
+.bg-white {
+  background-color: #fff;
+}
+
+.rounded-sm {
+  border-radius: 0.125rem;
 }
 </style>
