6f573f51cd11f9f6e367e93b34bb225f6316a739
daniel-le97 <daniel97.le@gmail.com>
2023-01-31 17:45:29 -0700

    products

--- server/api/cart/[id].put.ts
+++ server/api/cart/[id].put.ts
@@ -1,112 +1,104 @@
-import { getServerSession } from "#auth"
-
+/* eslint-disable no-return-assign */
+import { getServerSession } from '#auth'
 
 export default defineEventHandler(async (event) => {
-    const prisma = event.context.prisma
-  const {id} = getRouterParams(event)
-  const data =  await getServerSession(event)
+  const prisma = event.context.prisma
+  const { id } = getRouterParams(event)
+  const data = await getServerSession(event)
   const user = data?.user
- const cart = await  prisma.cart.findUnique({
-  where:{
-    userId: user.id
-  }
-})
-
-if (!cart) {
-  let newCart = await prisma.cart.create({
-    data:{
-      userId:user.id,
-
-      products:{
-        connect:{
-          id:parseInt(id)
-        }
-      }
-    },
-    include:{
-      products:{
-      }
+  const cart = await prisma.cart.findUnique({
+    where: {
+      userId: user.id
     }
   })
 
-   let cartTotal = 0
+  if (!cart) {
+    const newCart = await prisma.cart.create({
+      data: {
+        userId: user.id,
 
-newCart?.products?.forEach(p=> cartTotal += (p.price *
-  (p.quantity ||0 ) ))
-
-  return {newCart,cartTotal}
-
-}
-
-// if (event.node.req.method === 'DELETE') {
-//  let updatedCart = await  prisma.cart.update({
-//   where:{
-//     userId:user.id
-//   },
-// data:{
-//   products:{
-
-//     delete:{
-//       where:{
-//           id : parseInt(id)
-//       },
-
-//     }
-//   }
-// },
-// include:{
-//   products:{}
-// }
-// })
-
-// }
-
-
- let updatedCart = await  prisma.cart.update({
-  where:{
-    userId:user.id
-  },
-data:{
-  products:{
-    connect:{
-      id:parseInt(id)
-
-    },
-    update:{
-      where:{
-          id:parseInt(id)
+        products: {
+          connect: {
+            id: parseInt(id)
+          }
+        }
       },
-      data:{
-        quantity:{
-          increment:1
+      include: {
+        products: {
         }
       }
+    })
+
+    let cartTotal = 0
+
+    newCart?.products?.forEach(p => cartTotal += (p.price *
+  (p.quantity || 0)))
+
+    return { newCart, cartTotal }
+  }
+
+  // if (event.node.req.method === 'DELETE') {
+  //  let updatedCart = await  prisma.cart.update({
+  //   where:{
+  //     userId:user.id
+  //   },
+  // data:{
+  //   products:{
+
+  //     delete:{
+  //       where:{
+  //           id : parseInt(id)
+  //       },
+
+  //     }
+  //   }
+  // },
+  // include:{
+  //   products:{}
+  // }
+  // })
+
+  // }
+
+  const updatedCart = await prisma.cart.update({
+    where: {
+      userId: user.id
+    },
+    data: {
+      products: {
+        connect: {
+          id: parseInt(id)
+
+        },
+        update: {
+          where: {
+            id: parseInt(id)
+          },
+          data: {
+            quantity: {
+              increment: 1
+            }
+          }
+        }
+      }
+    },
+    include: {
+      products: {}
     }
+  })
+
+  if (updatedCart.products.length <= 0) {
+    await prisma.cart.delete({
+      where: {
+        userId: user.id
+      }
+    })
   }
-},
-include:{
-  products:{}
-}
-})
 
-if (updatedCart.products.length <= 0) {
+  let cartTotal = 0
 
+  updatedCart?.products?.forEach(p => cartTotal += (p.price *
+  (p.quantity || 0)))
 
-await prisma.cart.delete({
-  where:{
-    userId: user.id
-  }
-})
-
-}
-
-   let cartTotal = 0
-
-updatedCart?.products?.forEach(p=> cartTotal += (p.price *
-  (p.quantity ||0 ) ))
-
-  return {updatedCart,cartTotal}
-
-
-
+  return { updatedCart, cartTotal }
 })
