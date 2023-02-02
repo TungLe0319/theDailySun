77865c10ed5062600bb29729aa9c9f506d71e155
daniel-le97 <daniel97.le@gmail.com>
2023-02-01 22:26:36 -0700

    1

--- stores/CartsStore.js
+++ stores/CartsStore.js
@@ -26,6 +26,6 @@ export const useCartStore = defineStore('cart', {
   //   }
   // }
 })
-// if (import.meta.hot) {
-//   import.meta.hot.accept(acceptHMRUpdate(useCartStore, import.meta.hot))
-// }
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useCartStore, import.meta.hot))
+}
