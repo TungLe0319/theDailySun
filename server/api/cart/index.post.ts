83e178e443a93586c14db2936af42ba49dccbed2
daniel-le97 <daniel97.le@gmail.com>
2023-02-02 11:16:26 -0700

    update

--- server/api/cart/index.post.ts
+++ server/api/cart/index.post.ts
@@ -1,5 +1,48 @@
-export default defineEventHandler((event) => {
-  // const prisma = event.context.prisma
+import { Cart } from '@prisma/client'
+import { getServerSession } from '#auth'
+// import { never } from 'zod';
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+interface newProduct {
+  id: number;
+  quantity: number | any;
+}
+export default defineEventHandler(async (event) => {
+  const prisma = event.context.prisma
+  const session = await getServerSession(event)
+  const userId = session?.user.id
+  if (!userId) {
+    createError('you must be logged in to add products to a cart')
+  }
+  const body: newProduct = await readBody(event)
+  if (!body) {
+    createError('need to send in a body')
+  }
+  if (!body.quantity && !body.id) {
+    createError('error')
+  }
+  const cart: Cart = await prisma.cart.update({
+    where: {
+      userId
+    },
+    data: {
+      products: {
+        connect: {
+          id: body.id
+        },
+        update: {
+          where: {
+            id: body.id
+          },
+          data: {
+            quantity: body.quantity
+
+          }
+        }
+      }
+    }
+  })
+
   //   const { id } = event.context.body
 
   // let cart = await $trpc.cart.findUnique.useQuery({
