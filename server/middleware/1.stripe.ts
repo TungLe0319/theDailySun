9dd7c78a9765ac7a961a3ea38f67807a4b24c55b
daniel-le97 <daniel97.le@gmail.com>
2023-02-06 00:16:56 -0700

    checkout

---
+++ server/middleware/1.stripe.ts
@@ -0,0 +1,24 @@
+// import Stripe from "stripe";
+
+import Stripe from 'stripe'
+
+// import { loadStripe, Stripe } from '@stripe/stripe-js'
+
+let stripe : Stripe
+declare module 'h3' {
+  interface H3EventContext {
+    stripe: Stripe;
+  }
+}
+// let stripe: any
+export default eventHandler((event) => {
+  if (!stripe) {
+    // @ts-ignore
+    stripe = new Stripe(process.env.STRIPE_SK, { apiVersion: '2022-11-15' })
+  }
+  event.context.stripe = stripe
+})
+// @ts-ignore
+export function useStripe (event: H3Event) : Stripe {
+  return event.context.stripe
+}
