1c5e9bb3facc2fd87273506cbfa70128a15556c4
Tung N Le <tung.le0319@gmail.com>
2023-02-09 15:30:44 -0700

    tung

--- components/CreditCard.vue
+++ components/CreditCard.vue
@@ -1,215 +1,218 @@
 <template>
   <div class="component mt-5 flex flex-col justify-center items-center">
-<div class="text-center">
-  <h1 class="">You can use this card  information to do the stripe checkout.</h1>
-<h2 class="title mt-5">Hover to reveal 3-digit security code </h2>
-
-</div>
-
-<div class="card">
-  <div class="card__front card__part">
-
-
-    <p class="card_numer">4242424242424242</p>
-    <div class="card__space-75">
-      <span class="card__label">Card holder</span>
-      <p class="card__info">Your Name </p>
+    <div class="text-center">
+      <h1 class="">
+        You can use this card information to do the stripe checkout.
+      </h1>
+      <h2 class="title mt-5">Hover to reveal 3-digit security code</h2>
     </div>
-    <div class="card__space-25">
-      <span class="card__label">Expires</span>
-            <p class="card__info">8/26</p>
-    </div>
-  </div>
 
-  <div class="card__back card__part">
-    <div class="card__black-line"></div>
-    <div class="card__back-content">
-      <div class="card__secret">
-        <p class="card__secret--last">812</p>
+    <div class="card">
+      <div class="card__front card__part">
+        <p class="card_numer">4242424242424242</p>
+        <div class="card__space-75">
+          <span class="card__label">Card holder</span>
+          <p class="card__info">Your Name</p>
+        </div>
+        <div class="card__space-25">
+          <span class="card__label">Expires</span>
+          <p class="card__info">8/26</p>
+        </div>
       </div>
 
-
-
+      <div class="card__back card__part">
+        <div class="card__black-line"></div>
+        <div class="card__back-content">
+          <div class="card__secret">
+            <p class="card__secret--last">812</p>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-
-</div>
-
   </div>
 </template>
 
-
 <script>
 export default {
-  setup(){
-    return {}
-  }
-}
+  setup() {
+    return {};
+  },
+};
 </script>
-
 
 <style lang="scss" scoped>
 .title {
-    margin-bottom: 10px;
-    color: #162969;
+  margin-bottom: 10px;
+  color: #162969;
 }
 
-
-.card{
-width: 320px;
-height: 190px;
+.card {
+  width: 320px;
+  height: 190px;
   -webkit-perspective: 600px;
   -moz-perspective: 600px;
-  perspective:600px;
-
+  perspective: 600px;
 }
 
-.card__part{
+.card__part {
   box-shadow: 1px 1px #aaa3a3;
-    top: 0;
+  top: 0;
   position: absolute;
-z-index: 1000;
+  z-index: 1000;
   left: 0;
   display: inline-block;
-    width: 320px;
-    height: 190px;
-    background-image: url('https://imgs.search.brave.com/Nwem_XhbA3RylQ4pPDE2OuaxvbrLXxiHnqSD7U85r7Y/rs:fit:1200:600:1/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vY3NzLXRy/aWNrcy5jb20vd3At/Y29udGVudC91cGxv/YWRzLzIwMTcvMDkv/Z3VpZGUtZ3JhZGVp/bnQucG5nP2ZpdD0x/MjAwJTJDNjAwJnNz/bD0x'), linear-gradient(to right bottom, #fd696b, #fa616e, #f65871, #f15075, #ec4879); /*linear-gradient(to right bottom, #fd8369, #fc7870, #f96e78, #f56581, #ee5d8a)*/
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-    border-radius: 8px;
+  width: 320px;
+  height: 190px;
+  background-image: url("https://imgs.search.brave.com/Nwem_XhbA3RylQ4pPDE2OuaxvbrLXxiHnqSD7U85r7Y/rs:fit:1200:600:1/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vY3NzLXRy/aWNrcy5jb20vd3At/Y29udGVudC91cGxv/YWRzLzIwMTcvMDkv/Z3VpZGUtZ3JhZGVp/bnQucG5nP2ZpdD0x/MjAwJTJDNjAwJnNz/bD0x"),
+    linear-gradient(
+      to right bottom,
+      #fd696b,
+      #fa616e,
+      #f65871,
+      #f15075,
+      #ec4879
+    ); /*linear-gradient(to right bottom, #fd8369, #fc7870, #f96e78, #f56581, #ee5d8a)*/
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  border-radius: 8px;
 
-    -webkit-transition: all .5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-    -moz-transition: all .5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-    -ms-transition: all .5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-    -o-transition: all .5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-    transition: all .5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-    -webkit-transform-style: preserve-3d;
-    -moz-transform-style: preserve-3d;
-    -webkit-backface-visibility: hidden;
-    -moz-backface-visibility: hidden;
+  -webkit-transition: all 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  -moz-transition: all 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  -ms-transition: all 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  -o-transition: all 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  transition: all 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  -webkit-transform-style: preserve-3d;
+  -moz-transform-style: preserve-3d;
+  -webkit-backface-visibility: hidden;
+  -moz-backface-visibility: hidden;
 }
 
-.card__front{
+.card__front {
   padding: 18px;
--webkit-transform: rotateY(0);
--moz-transform: rotateY(0);
+  -webkit-transform: rotateY(0);
+  -moz-transform: rotateY(0);
 }
 
 .card__back {
   padding: 18px 0;
--webkit-transform: rotateY(-180deg);
--moz-transform: rotateY(-180deg);
+  -webkit-transform: rotateY(-180deg);
+  -moz-transform: rotateY(-180deg);
 }
 
 .card__black-line {
-    margin-top: 5px;
-    height: 38px;
-    background-color: #303030;
+  margin-top: 5px;
+  height: 38px;
+  background-color: #303030;
 }
 
 .card__logo {
-    height: 16px;
+  height: 16px;
 }
 
-.card__front-logo{
-      position: absolute;
-    top: 18px;
-    right: 18px;
+.card__front-logo {
+  position: absolute;
+  top: 18px;
+  right: 18px;
 }
 .card__square {
-    border-radius: 5px;
-    height: 30px;
+  border-radius: 5px;
+  height: 30px;
 }
 
 .card_numer {
-    display: block;
-    width: 100%;
-    word-spacing: 4px;
-    font-size: 20px;
-    letter-spacing: 2px;
-    color: #fff;
-    text-align: center;
-    margin-bottom: 20px;
-    margin-top: 20px;
+  display: block;
+  width: 100%;
+  word-spacing: 4px;
+  font-size: 20px;
+  letter-spacing: 2px;
+  color: #fff;
+  text-align: center;
+  margin-bottom: 20px;
+  margin-top: 20px;
 }
 
 .card__space-75 {
-    width: 75%;
-    float: left;
+  width: 75%;
+  float: left;
 }
 
 .card__space-25 {
-    width: 25%;
-    float: left;
+  width: 25%;
+  float: left;
 }
 
 .card__label {
-    font-size: 10px;
-    text-transform: uppercase;
-    color: rgba(255,255,255,0.8);
-    letter-spacing: 1px;
+  font-size: 10px;
+  text-transform: uppercase;
+  color: rgba(255, 255, 255, 0.8);
+  letter-spacing: 1px;
 }
 
 .card__info {
-    margin-bottom: 0;
-    margin-top: 5px;
-    font-size: 16px;
-    line-height: 18px;
-    color: #fff;
+  margin-bottom: 0;
+  margin-top: 5px;
+  font-size: 16px;
+  line-height: 18px;
+  color: #fff;
   letter-spacing: 1px;
   text-transform: uppercase;
 }
 
 .card__back-content {
-    padding: 15px 15px 0;
+  padding: 15px 15px 0;
 }
 .card__secret--last {
-    color: #303030;
-    text-align: right;
-    margin: 0;
-    font-size: 14px;
+  color: #303030;
+  text-align: right;
+  margin: 0;
+  font-size: 14px;
 }
 
 .card__secret {
-    padding: 5px 12px;
-    background-color: #fff;
-    position:relative;
+  padding: 5px 12px;
+  background-color: #fff;
+  position: relative;
 }
 
-.card__secret:before{
-  content:'';
+.card__secret:before {
+  content: "";
   position: absolute;
-top: -3px;
-left: -3px;
-height: calc(100% + 6px);
-width: calc(100% - 42px);
-border-radius: 4px;
-  background: repeating-linear-gradient(45deg, #ededed, #ededed 5px, #f9f9f9 5px, #f9f9f9 10px);
+  top: -3px;
+  left: -3px;
+  height: calc(100% + 6px);
+  width: calc(100% - 42px);
+  border-radius: 4px;
+  background: repeating-linear-gradient(
+    45deg,
+    #ededed,
+    #ededed 5px,
+    #f9f9f9 5px,
+    #f9f9f9 10px
+  );
 }
 
 .card__back-logo {
-    position: absolute;
-    bottom: 15px;
-    right: 15px;
+  position: absolute;
+  bottom: 15px;
+  right: 15px;
 }
 
 .card__back-square {
-    position: absolute;
-    bottom: 15px;
-    left: 15px;
+  position: absolute;
+  bottom: 15px;
+  left: 15px;
 }
 
 .card:hover .card__front {
-    -webkit-transform: rotateY(180deg);
-    -moz-transform: rotateY(180deg);
-    transition: all 0.5s ease !important
-
+  -webkit-transform: rotateY(180deg);
+  -moz-transform: rotateY(180deg);
+  transition: all 0.5s ease !important;
 }
 
 .card:hover .card__back {
-    -webkit-transform: rotateY(0deg);
-    -moz-transform: rotateY(0deg);
-    transition: all 0.5s ease !important;
+  -webkit-transform: rotateY(0deg);
+  -moz-transform: rotateY(0deg);
+  transition: all 0.5s ease !important;
 }
 </style>
